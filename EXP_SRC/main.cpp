3f8f154e39aac3ea813c73d46e2dfc3cff000a87
danyragno00 <75252509+danyragno00@users.noreply.github.com>
2020-12-01 10:26:44 +0100

    Add files via upload

--- EXP_SRC/main.cpp
+++ EXP_SRC/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+
 using namespace std;
 
+bool credito_sufficiente (int puntata)
+
+{
+   int credito = 20;
+if (puntata >= credito)
+    return true;
+
+  else
+    return false;
+
+}
 
 
 
 int main()
 {
-    return 0;
+  int credito, credito_s;
+  cout << "inserisci il credito che vuoi scommetere: " << endl;
+       cin >> credito;
+  credito_s = credito_sufficiente(credito);
+  if (credito_s == false)
+     cout << "il credito inserito non è sufficiente per la scommessa..." << endl;
+  if (credito_s == true)
+     cout << "il credito inserito è sufficiente per la scommessa!" << endl;
+
+
+return 0;
 }
