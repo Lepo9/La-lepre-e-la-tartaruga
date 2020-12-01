043ed8ac9189f177c1dbdd14f7b3be13898bff0e
Lepo9 <72271255+Lepo9@users.noreply.github.com>
2020-12-01 10:28:07 +0100

    Update main.cpp

--- EXP_SRC/main.cpp
+++ EXP_SRC/main.cpp
@@ -3,30 +3,17 @@
 using namespace std;
 
 bool credito_sufficiente (int puntata)
-
 {
    int credito = 20;
-if (puntata >= credito)
-    return true;
-
-  else
-    return false;
-
+   if (puntata >= credito)
+      return true;
+   else
+      return false;
 }
 
 
 
 int main()
 {
-  int credito, credito_s;
-  cout << "inserisci il credito che vuoi scommetere: " << endl;
-       cin >> credito;
-  credito_s = credito_sufficiente(credito);
-  if (credito_s == false)
-     cout << "il credito inserito non è sufficiente per la scommessa..." << endl;
-  if (credito_s == true)
-     cout << "il credito inserito è sufficiente per la scommessa!" << endl;
-
-
 return 0;
 }
