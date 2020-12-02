2ab7cc55ef7bdf80c928ac7e2a7bfede10f2d957
Lepo9 <72271255+Lepo9@users.noreply.github.com>
2020-12-02 12:04:35 +0100

    0.3

--- EXP_SRC/main.cpp
+++ EXP_SRC/main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 //Progetto Informatica
 //Autori: Giacomo Garoffoli, Marco Leporati, Daniele Ragnoli, Matteo Betella
-//Revisione 0.2
+//Revisione 0.3
 
-//positivo assume valore true se il numero è postivo altrimenti false
+//se positivo restituisce valore true, se il numero è postivo altrimenti false
 bool positivo (int n)
 {
     if (n>0)    //controllo se n è positivo
@@ -18,18 +18,15 @@ bool positivo (int n)
 }
 
 
-//Funzione per stampare i numeri di spazi (utile nel disegno)
-int spazi (int n)
+//la funzione stampa un numero di spazi n (utile nel disegno)
+void spazi (int n)
 {
     for (int i = 0; i < n; i ++) //numero giri = n
-  {
-     cout << " ";
-  }
-return 0;
+        cout << " ";
 }
 
 
-//Funzione per controllare se il credito è sufficente per la puntata
+//la funzione restituisce true se il credito è sufficiente altrimenti false
 bool credito_sufficiente (int credito, int puntata)
 {
     if (credito >=puntata)
@@ -38,21 +35,44 @@ bool credito_sufficiente (int credito, int puntata)
         return false; //Il credito non è sufficiente
 }
 
-
+//la funzione genera un numero corrispondente al numero di passi che la 
+//tartaruga deve fare tenendo conto delle probabilità
 int spostamento_tarta ()
 {
     int n;  //variabile di appoggio
 
     n = rand () % 10 + 1;    //genera un numero da 1 a 10
 
-    if (n <= 5)         //5/10 possibilità sono il 50%
-        return 3;        //arrancata rapida
-    else if (n <= 7)    //2/10 possibilità sono il 20%
-        return -6;       //scivolone
-    else                //(10-5-2)/10 possibilità sono il 30%
-        return 1;        //arrancata lenta
+    if (n <= 5)         //  5/10 possibilità sono il 50%
+        return 3;       //  arrancata rapida
+    else if (n <= 7)    //  2/10 possibilità sono il 20%
+        return -6;      //  scivolone
+    else                //  (10-5-2)/10 possibilità sono il 30%
+        return 1;       //  arrancata lenta
 }
 
+
+//la funzione genera un numero corrispondente al numero di passi che la 
+//lepre deve fare tenendo conto delle probabilità
+int spostamento_lepre () 
+{
+    int n;  //variabile di appoggio
+
+    n = rand () % 10 + 1;    //genera un numero da 1 a 10
+
+    if (n <= 2)         //  2/10 possibilità sono il 20%
+        return 0;       //  dormita
+    else if (n <= 4)    //  (4-2)/10 possibilità sono il 20%
+        return 9;       //  salto lungo
+    else if (n = 5)     //  1/10 possibilità sono il 10%
+        return -12;     //  scivolone lungo
+    else if (n <= 8)    //  (8-5)/10 possibilità sono il 30%
+        return 1;       //  salto corto
+    else                //  (10-8)/10 possibilità sono il 20%
+        return -2;      //  scivolone corto
+}
+    
+    
 int gettoni_vinti(int gettoni_puntati, int flag_scommessa, int flag_vincitore)
 {
     //FLAG_SCOMMESSA (1 LEPRE - 0 TARTARUGA)
